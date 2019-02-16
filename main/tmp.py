68f5644ee065162be2454e9b93c780228412386f
Syncla <vvazir@udel.edu>
2019-02-16 03:15:03 -0500

    lotsa stuff

--- main/tmp.py
+++ main/tmp.py
@@ -1,4 +1,4 @@
-import os, sys, inspect, time
+import os, sys, thread, inspect, time
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 # Windows and Linux
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
@@ -6,5 +6,4 @@ arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 #arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
 
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
-print(sys.path)
 import Leap
