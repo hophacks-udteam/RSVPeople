c39bfa6ba60357b6de8d36c0beab40c091ed6a92
Syncla <vvazir@udel.edu>
2019-02-16 00:13:55 -0500

    added a test file

---
+++ src/tmp.py
@@ -0,0 +1,5 @@
+import os, sys, inspect
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+lib_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+sys.path.insert(0, lib_dir)
+import Leap
